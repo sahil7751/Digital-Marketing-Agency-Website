f121a13b6981b9f8bb878e4d50adb9e6beb5bb31
Sahil Jirapure <sahiljirapure8@gmail.com>
2026-07-10 23:46:19 +0530

    updated

--- src/components/ui/container.tsx
+++ src/components/ui/container.tsx
@@ -1,5 +1,5 @@
 import type { ComponentPropsWithoutRef } from "react";
 
 export function Container({ className = "", ...props }: ComponentPropsWithoutRef<"div">) {
-  return <div className={`mx-auto w-full max-w-7xl px-5 sm:px-8 lg:px-10 ${className}`} {...props} />;
+  return <div className={`mx-auto w-full max-w-7xl px-6 sm:px-8 lg:px-10 ${className}`} {...props} />;
 }
