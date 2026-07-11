8edc3775fdf809663095bbfcf6909b1a145ac582
Sahil Jirapure <sahiljirapure8@gmail.com>
2026-07-11 12:23:01 +0530

    added

--- src/components/theme/theme-provider.tsx
+++ src/components/theme/theme-provider.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { MotionConfig } from "framer-motion";
 import type { ComponentProps } from "react";
 
 export function ThemeProvider({ children, ...props }: ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange {...props}>{children}</NextThemesProvider>;
+  return <MotionConfig reducedMotion="user"><NextThemesProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange {...props}>{children}</NextThemesProvider></MotionConfig>;
 }
